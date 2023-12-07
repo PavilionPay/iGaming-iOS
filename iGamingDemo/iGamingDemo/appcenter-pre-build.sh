e71f541e8298d4636897278c0e0f0c3fc4345206
James Wright <james.wright@insight.com>
2023-12-07 14:18:34 -0500

    Added build phase to add associated domain to entitlements

--- iGamingDemo/iGamingDemo/appcenter-pre-build.sh
+++ iGamingDemo/iGamingDemo/appcenter-pre-build.sh
@@ -1,10 +1,13 @@
 
-echo "Building out custom EnvironmentVariables.plist..."
+echo "Building out custom BuildEnvironmentVariables.plist..."
 plutil -replace RedirectUri -string "$RedirectUri" $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 plutil -replace BaseUri -string "$BaseUri" $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 plutil -replace Audience -string "$Audience" $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 plutil -replace Issuer -string "$Issuer" $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 plutil -replace Secret -string "$Secret" $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
+
+# Append to the "AssociatedDomains" array in the bundle plist
+plutil -insert AssociatedDomains -string "$AssociatedDomain" -append $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 
 plutil -p $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/BuildEnvironmentVariables.plist
 
