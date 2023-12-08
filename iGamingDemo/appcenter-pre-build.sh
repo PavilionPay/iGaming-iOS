30fd8c0d7632b9cc5017f1a3c722feab0c60769e
James Wright <james.wright@insight.com>
2023-12-08 16:54:28 -0500

    Update appcenter-pre-build.sh

--- iGamingDemo/appcenter-pre-build.sh
+++ iGamingDemo/appcenter-pre-build.sh
@@ -7,7 +7,7 @@ plutil -replace Issuer      -string "$Issuer"      $APPCENTER_SOURCE_DIRECTORY/i
 plutil -replace Secret      -string "$Secret"      $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/iGamingDemo/BuildEnvironmentVariables.plist
 
 # Append to the "AssociatedDomains" array in the bundle plist
-plutil -insert AssociatedDomains -string "$AssociatedDomain" -append $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/iGamingDemo/BuildEnvironmentVariables.plist
+plutil -insert AssociatedDomains -string "$AssociatedDomains" -append $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/iGamingDemo/BuildEnvironmentVariables.plist
 
 plutil -p $APPCENTER_SOURCE_DIRECTORY/iGamingDemo/iGamingDemo/BuildEnvironmentVariables.plist
 
